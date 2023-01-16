9cc2b93278c4aeef9ea132adda4e0eb575129b02
levklg <myevent@mail.ru>
2023-01-16 10:47:24 +0300

    commit

--- src/main/java/com/exemple/service/ProcessingMapImp.java
+++ src/main/java/com/exemple/service/ProcessingMapImp.java
@@ -1,5 +1,9 @@
 package com.exemple.service;
 
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProcessingMapImp implements ProcessingMap {
@@ -9,16 +13,34 @@ public class ProcessingMapImp implements ProcessingMap {
 
     @Override
     public boolean compareMaps(Map<String, String> newNews, Map<String, String> oldNews) {
-        return false;
+        var isSameMaps = oldNews.entrySet().stream()
+                .allMatch(e -> e.getKey().equals(newNews.get(e.getKey())));
+
+        return isSameMaps;
+
     }
 
     @Override
     public Map<String, String> getMapOfNewsUpdates(Map<String, String> newNews, Map<String, String> oldNews) {
-        return null;
+        Map <String, String> updateMapNews = new HashMap<>();
+
+        MapDifference<String, String> diff = Maps.difference(oldNews, newNews);
+        updateMapNews = diff.entriesOnlyOnRight();
+
+        return  updateMapNews;
+
     }
 
     @Override
     public Map<String, String> mergeMapNewsUpdates(Map<String, String> newNews, Map<String, String> oldNews) {
-        return null;
+        Map<String, String> mapMerge = new HashMap<>();
+        if(!newNews.isEmpty() || !oldNews.isEmpty()) {
+            MapDifference<String, String> diff = Maps.difference(oldNews, newNews);
+            var oldMapNews = diff.entriesInCommon();
+            var updateMapNews = diff.entriesOnlyOnRight();
+            mapMerge.putAll(oldMapNews);
+            mapMerge.putAll(updateMapNews);
+        }
+        return mapMerge;
     }
 }
